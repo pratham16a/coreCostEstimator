32647ca3038587dbd7e4e311be8a0730b9ba302e
pratham16a <111959587+pratham16a@users.noreply.github.com>
2023-07-31 21:28:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.locals._ = _;
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser : True});
+    const conn = await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser : true});
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
